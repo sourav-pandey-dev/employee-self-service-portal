ace05b6940ff3706f41c9a084e9dba68ce7b258f
RakeshMundel <rakeshmundel000@gmail.com>
2026-06-09 15:06:21 +0530

    Structure Added

---
+++ employee-self-service-portal/src/app/shared/components/calendar-view/calendar-view.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CalendarView } from './calendar-view';
+
+describe('CalendarView', () => {
+  let component: CalendarView;
+  let fixture: ComponentFixture<CalendarView>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalendarView],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalendarView);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ employee-self-service-portal/src/app/shared/components/calendar-view/calendar-view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-view',
+  imports: [],
+  templateUrl: './calendar-view.html',
+  styleUrl: './calendar-view.css',
+})
+export class CalendarView {}
